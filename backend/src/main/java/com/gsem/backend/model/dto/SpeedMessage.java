81bad1316b2ed67d7d052fc034e7ea2cdcefc457
김현호 <rlagusgh2199@naver.com>
2025-05-08 04:14:06 +0900

    "초기 프로젝트 구조 설정"

---
+++ backend/src/main/java/com/gsem/backend/model/dto/SpeedMessage.java
@@ -0,0 +1,13 @@
+package com.gsem.backend.model.dto;
+
+public class SpeedMessage {
+    private int speed;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
